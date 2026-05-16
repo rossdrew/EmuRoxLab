d454efae6ddee85a611f12e16bfc6c6464d14733
rossdrew <thek1ng13@hotmail.com>
2026-05-16 23:19:59 +0100

    Some movement towards cycle accurate

--- src/main/java/com/rox/MOS6502.java
+++ src/main/java/com/rox/MOS6502.java
@@ -1,19 +1,84 @@
 package com.rox;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+
+import static com.rox.MOS6502.MicroOp.*;
+
 public class MOS6502 implements ClockWatcher {
     private final MemoryBus memoryBus;
 
     private int pc;
+
+    private int rInstruction = 0;
+    private int rAccumulator = 0;
+    private boolean fCarry = false;
+
+    private Deque<MicroOp> opStack = new ArrayDeque<>();
+
+    enum MicroOp {
+        IR_FROM_PC_ADDRESS, //IMPLICIT!! PC -> r_instruction
+
+        Z_ADDRESS_FROM_PC_ADDRESS,
+        Z_ADDRESS_READ,
+
+        OP,
+        LOW_ADDRESS_FROM_PC_ADDRESS, //PC -> low address
+        HIGH_ADDRESS_FROM_PC_ADDRESS, //PC -> high address
+
+        ADJUSTED_ADDRESS, // read adjusted address
+        PAGE_CROSS_CYCLE // optional extra page-cross cycle
+    }
+
+    public enum OpCode {
+        ADC_Z (0x65, Arrays.asList(Z_ADDRESS_FROM_PC_ADDRESS, Z_ADDRESS_READ, OP));
+
+        private final int id;
+        private final List<MicroOp> microOperations;
+
+        OpCode(final int id, final List<MicroOp> microOperations) {
+            this.id = id;
+            this.microOperations = microOperations;
+        }
+
+        public int id(){
+            return id;
+        }
+
+        public static OpCode of(final int id) {
+            return ADC_Z;
+        }
+    }
 
     public MOS6502(final MemoryBus memoryBus) {
         this.memoryBus = memoryBus;
         pc = 0;
     }
 
+    /** Overflow safe PC + increment */
+    private int pc(){
+        int cached_pc = pc;
+        pc = (pc + 1) & 0xFFFF;
+        return cached_pc;
+    }
+
     @Override
     public void tick() {
-        int instruction = memoryBus.read(pc++);
-        //decode it
-        //execute it
+        if (opStack.isEmpty()){
+            //Load opcode into opstack
+            rInstruction = memoryBus.read(pc()); //read
+            final OpCode opcode = OpCode.of(rInstruction); //decode
+            opcode.microOperations.reversed().forEach(microop -> opStack.push(microop)); //schedule
+        }else {
+            //Execute opcode/opstack
+            final MicroOp op = opStack.pop();
+
+            //1. getmemory at pc, 2. increment pc
+
+            System.out.println(op.name());
+            //TODO execute microop
+        }
     }
 }
--- src/test/java/com/rox/MOS6502Test.java
+++ src/test/java/com/rox/MOS6502Test.java
@@ -5,6 +5,7 @@ import net.jqwik.api.Property;
 import net.jqwik.api.constraints.IntRange;
 import org.junit.jupiter.api.Test;
 
+import static com.rox.MOS6502.OpCode.ADC_Z;
 import static org.mockito.Mockito.*;
 
 public class MOS6502Test {
@@ -29,4 +30,20 @@ public class MOS6502Test {
 
         verify(bus, times(ticks)).read(anyInt());
     }
+
+    @Test
+    public void opCode(){
+        final MemoryBus bus = mock(MemoryBus.class);
+        when (bus.read(0)).thenReturn(ADC_Z.id());//ADC Zero Page
+        when (bus.read(1)).thenReturn(10);//Argument - Address in Zero Page
+
+        final MOS6502 cp = new MOS6502(bus);
+
+        cp.tick(); //Load OpCode
+        cp.tick(); //Load Argument
+        cp.tick(); //Read Zero Page
+
+        verify(bus, times(1)).read(0);
+    }
 }
+
