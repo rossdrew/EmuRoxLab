66a3ed4f9eb3cf76d89ba384ecb7438834e27500
rossdrew <thek1ng13@hotmail.com>
2026-07-31 12:57:02 +0100

    Reminder of possible future applicability

--- src/main/java/com/rox/mem/OamDmaBus.java
+++ src/main/java/com/rox/mem/OamDmaBus.java
@@ -1,10 +1,12 @@
 package com.rox.mem;
 
 /**
- * A {@link MemoryBus} that can also accept an OAM DMA transfer ({@code $4014}): 256 bytes copied
+ * Currently, A {@link MemoryBus} that can also accept an OAM DMA transfer ({@code $4014}): 256 bytes copied
  * straight into sprite memory in one shot, rather than one byte at a time through the normal register
  * interface. Lets {@link NESMemoryBus} drive a DMA into the PPU without depending on the concrete
  * {@code PPU} class.
+ *
+ * XXX: In future this could be used for any bulk data bus if required
  */
 public interface OamDmaBus extends MemoryBus {
     /** Copy 256 bytes into OAM, starting at the current OAM address, wrapping mod 256. */
